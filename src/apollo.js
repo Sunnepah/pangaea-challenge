96ea1fbed3ebd83029a0094cb0dbebeffe292015
fisayomi <fisayomiadebajo@gmail.com>
2020-11-24 13:48:35 +0100

    initialize cart items in apollo cache

--- src/apollo.js
+++ src/apollo.js
@@ -1,8 +1,22 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, InMemoryCache, makeVar } from '@apollo/client';
+
+export const cartItemsVar = makeVar([]);
 
 const client = new ApolloClient({
   uri: 'https://pangaea-interviews.now.sh/api/graphql',
-  cache: new InMemoryCache(),
+  cache: new InMemoryCache({
+    typePolicies: {
+      Query: {
+        fields: {
+          cartItems: {
+            read() {
+              return cartItemsVar();
+            },
+          },
+        },
+      },
+    },
+  }),
 });
 
 export default client;
