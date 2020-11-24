1389067e977bbe979b242900f31987f176d3dd8f
fisayomi <fisayomiadebajo@gmail.com>
2020-11-24 14:54:56 +0100

    display number of cart items

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import { cartItemsVar } from '../apollo';
+import { useReactiveVar } from '@apollo/client';
 
 const Header = () => {
+  const numberOfCartItems = useReactiveVar(cartItemsVar).length;
+
   return (
     <div className="header">
       <p className="logo">LUMIN</p>
@@ -16,6 +20,9 @@ const Header = () => {
       <div className="header__account">
         <p className="header__account--text">Account</p>
         <FontAwesomeIcon icon={faShoppingCart} />
+        {!!numberOfCartItems && (
+          <p className="cart-count">{numberOfCartItems}</p>
+        )}
       </div>
     </div>
   );
